cee1bd9b55f7d0de9c13894fd03c8d37d7468973
service.egs <service.egs@gmail.com>
2022-07-20 12:43:51 +0800

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UserdataComponent } from './pages/userdata/userdata.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, title: '首頁' },
+  { path: 'login', component: LoginComponent, title: '登入' },
+  { path: 'register', component: RegisterComponent, title: '註冊' },
+  { path: 'profile', component: ProfileComponent, title: 'Profile' },
+  { path: 'userdata', component: UserdataComponent, title: '使用者資料' },
+  { path: 'order', component: OrderComponent, title: '訂單' },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
+
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`My App - ${title}`);
+    }
+  }
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy }
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserdataComponent } from './pages/userdata/userdata.component';
+import { OrderComponent } from './pages/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    HomeComponent,
+    UserdataComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
